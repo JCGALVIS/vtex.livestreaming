4b0687b08fd375e946b5f0d5ee3c85e1c96bd004
Carlos Marriaga <andres07franco@gmail.com>
2021-06-23 16:52:04 -0500

    add useWebSocket

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,12 +1,13 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useMemo, useEffect } from 'react'
 import Button from '@vtex/styleguide/lib/Button'
 import Input from '@vtex/styleguide/lib/Input'
+import { uuid } from 'uuidv4'
 
-//import { Message } from './../../typings/livestreaming'
 import MessageLivestreamingIcon from '../icons/MessageLivestreamingIcon'
 import ArrorRightLivestreaming from '../icons/ArrorRightLivestreaming'
-
 import styles from './../../styles.module.css'
+import useWebSocket from '../../hooks/useWebSocket'
+import MessageRenderer from './MessageRenderer'
 
 type ChatProps = {
   title: string
@@ -16,6 +17,57 @@ type ChatProps = {
 export const Chat = ({ title, placeholder }: ChatProps) => {
   const chatAreaRef = useRef<HTMLDivElement>(null)
   const [content, setContent] = useState<string>('')
+  const { socket, chat, setChat } = useWebSocket()
+
+  const chats = [
+    {
+      sessionId: '347291293i1293912301',
+      data: 'genial',
+      sendDate: '25 de mayo',
+      username: 'Anonimo 67'
+    }
+  ]
+
+  const handlerSendMessage = async (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    event.persist()
+    const isEmpty = !(content !== null && content.trim() !== '')
+
+    if (isEmpty || !socket) {
+      return
+    }
+
+    const id = uuid()
+    const data = {
+      action: 'sendmessage',
+      data: content.replace(/\\/g, '\\\\').replace(/"/g, '\\"'),
+      sessionId: id,
+      username: undefined
+    }
+
+    socket.send(JSON.stringify(data))
+    setContent('')
+  }
+
+  const ChatMessages = useMemo(
+    () => MessageRenderer(chats || [], chat),
+    [/* chats, */ chat]
+  )
+
+  useEffect(() => {
+    if (chatAreaRef?.current) {
+      const current = chatAreaRef?.current
+
+      current.scrollBy(0, current.scrollHeight)
+      setTimeout(() => {
+        current.scrollBy(0, current.scrollHeight)
+      }, 300)
+    }
+  }, [ChatMessages])
+
+  useEffect(() => {
+    if (setChat) setChat([])
+  }, [/* chats, */ setChat])
 
   return (
     <div className={styles.container}>
@@ -25,9 +77,9 @@ export const Chat = ({ title, placeholder }: ChatProps) => {
       </div>
       <div className={styles.subContainer}>
         <div className={styles.chatArea} ref={chatAreaRef}>
-          {/* {ChatMessages} */}
+          {ChatMessages}
         </div>
-        <form className={styles.inputChat}>
+        <form onSubmit={handlerSendMessage} className={styles.inputChat}>
           <Input
             placeholder={placeholder}
             name='content'
---
+++ src/hooks/useWebSocket.ts
@@ -0,0 +1,86 @@
+import { useEffect, useState, useCallback } from 'react'
+import { uuid } from 'uuidv4'
+// eslint-disable-next-line no-unused-vars
+import { Message } from './../typings/livestreaming'
+
+const wssStream = 'wss://ouirrjizx5.execute-api.us-east-1.amazonaws.com/Prod'
+
+const useWebSocket = () => {
+  const [socket, setSocket] = useState<WebSocket>()
+  const [chat, setChat] = useState<Message[]>([])
+  const [isConnected, setIsConnected] = useState<boolean>(false)
+
+  const createWebSocket = useCallback(() => {
+    if (!wssStream) return
+
+    const connection = new WebSocket(wssStream as string)
+
+    connection.onopen = () => {
+      setIsConnected(true)
+    }
+
+    connection.onclose = () => {
+      setIsConnected(false)
+      setSocket(undefined)
+      createWebSocket()
+    }
+
+    connection.onerror = () => {
+      connection.close()
+    }
+
+    connection.onmessage = (event: MessageEvent) => {
+      const { action, data, sendDate, username } = JSON.parse(event.data)
+
+      switch (action) {
+        case 'sendmessage':
+          setChat((prev) => [...prev, { data, username, sendDate }])
+          break
+
+        default:
+          break
+      }
+    }
+
+    setSocket(connection)
+  }, [wssStream])
+
+  const sendAccountId = useCallback(() => {
+    if (!isConnected || !socket) return
+    const id = uuid()
+    socket.send(
+      JSON.stringify({
+        action: 'sendaccountid',
+        data: id,
+        username: 'undefined'
+      })
+    )
+  }, [isConnected, socket])
+
+  useEffect(() => {
+    if (!socket) return () => {}
+
+    return () => {
+      socket.close()
+    }
+  }, [socket])
+
+  useEffect(() => {
+    createWebSocket()
+  }, [createWebSocket])
+
+  useEffect(() => {
+    if (sendAccountId) {
+      sendAccountId()
+    }
+  }, [sendAccountId])
+
+  return {
+    socket,
+    chat,
+    setChat,
+    sendAccountId
+  }
+}
+
+export default useWebSocket
