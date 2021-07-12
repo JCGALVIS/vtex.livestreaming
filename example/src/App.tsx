dab6db15a1680447862741492ed9e91c62eeab7b
Juan Carlos Galvis Rodriguez <jcgalvis6@hotmail.com>
2021-07-12 16:58:36 -0500

    fix: Adjust component styles

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,12 +5,8 @@ import 'vtex.livestreaming/dist/index.css'
 import './app.css'
 
 const App = () => {
-
   return (
-    <Livestreaming
-      idLivestreaming='a48e402d-a67d-43f1-9a20-aea32d898355'
-      account='livestreamingdemo'
-    />
+    <Livestreaming idLivestreaming='__IDLIVESTREAMING' account='__ACCOUNT' />
   )
 }
 
