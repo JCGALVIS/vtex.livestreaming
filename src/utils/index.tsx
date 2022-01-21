d7698ae553900cf0906d8bbbfe0dfac15e42c231
Edwin Miranda <edwincolombo@hotmail.com>
2022-01-21 13:56:52 -0500

    send account when consulting platform products

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -5,7 +5,7 @@ import { getMobileOS, getDeviceType } from './getMobileOs'
 import { getRandomColor } from './getRandomColor'
 import Queue from './Queue'
 import { getRandomNumber } from './getRamdonNumber'
-
+import { base64Encode, base64Decode } from './base64'
 export {
   addToCart,
   calcHeightApp,
@@ -14,5 +14,7 @@ export {
   getMobileOS,
   getRandomColor,
   Queue,
-  getRandomNumber
+  getRandomNumber,
+  base64Encode,
+  base64Decode
 }
